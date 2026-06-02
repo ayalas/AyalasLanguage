cdd26fd74d53c63a13a25a1603e71a9e0d85f4e3
Ayala Swisa <shani.ayala@gmail.com>
2026-06-02 21:23:25 +0300

    add react compiler

--- AyalasLanguageWeb/vite.config.js
+++ AyalasLanguageWeb/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), // Standard React fast refresh
+    babel({
+      presets: [reactCompilerPreset()] // Handles the React Compiler automatically
+    })
+  ],
   server: {
     proxy: {
       '/api/': {
