e72b2fe3cc486278d45cda4a85d02ac08856fb16
Ayala Swisa <shani.ayala@gmail.com>
2026-06-12 09:40:19 +0300

    deal globally with websocket errors in vitest

--- AyalasLanguageWeb/vitest.setup.ts
+++ AyalasLanguageWeb/vitest.setup.ts
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+
+// Fix for "TypeError: The 'event' argument must be an instance of Event."
+// This ensures that the global Event and MessageEvent classes match 
+// what JSDOM/undici expect.
+Object.defineProperty(global, 'Event', { value: window.Event, configurable: true });
+Object.defineProperty(global, 'MessageEvent', { value: window.MessageEvent, configurable: true });
+
+// If the error persists, you might also need to sync the WebSocket itself
+Object.defineProperty(global, 'WebSocket', {
+  value: window.WebSocket,
+  configurable: true,
+});
