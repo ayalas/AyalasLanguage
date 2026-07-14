d6b19d6a3b799b51333e7102e8c53261b8f234d0
Ayala Swisa <shani.ayala@gmail.com>
2026-07-14 14:45:54 +0300

    fix turbo issue - 2.10.0 is the latest for current machine

--- apps/AyalasLanguageApp/app/(public)/(auth)/_layout.tsx
+++ apps/AyalasLanguageApp/app/(public)/(auth)/_layout.tsx
@@ -1,7 +1,36 @@
-import { Slot } from 'expo-router'
+import { Tabs } from 'expo-router'
 
 export default function PublicAuthLayout() {
   return (
-    <Slot/>
+    <Tabs screenOptions={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarItemStyle: {
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+            },
+            tabBarStyle: {
+                backgroundColor: "#0F0D23",
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: "absolute",
+                overflow: "hidden",
+                borderWidth: 1,
+                borderColor: "#0F0D23",
+            },
+        }}>
+            <Tabs.Screen name="login"
+                options={{
+                    title: "Log In"
+                }} />
+            <Tabs.Screen name="signup"
+                options={{
+                    title: "Sign Up"
+                }} />
+      </Tabs>
   )
 }
---
+++ apps/AyalasLanguageApp/app/(secured)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="home-container">
+      <Text className="learning-path-empty">It looks like there are not yet any lessons in this language.</Text>
+    </View>
+  );
+}
