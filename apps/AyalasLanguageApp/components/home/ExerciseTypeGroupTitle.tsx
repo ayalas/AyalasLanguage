03f0c3b09f7c45291c0b055faa45125f220f5bf3
Ayala Swisa <shani.ayala@gmail.com>
2026-07-16 18:05:53 +0300

    fix css

--- apps/AyalasLanguageApp/components/home/ExerciseTypeGroupTitle.tsx
+++ apps/AyalasLanguageApp/components/home/ExerciseTypeGroupTitle.tsx
@@ -5,7 +5,7 @@ import { View, Text } from "react-native";
 
 export function ExerciseTypeGroupTitle({ exerciseTypeId }: { exerciseTypeId: 0 | ExerciseType }) {
     return (
-        <View className="form-row">
+        <View className="home-header-row">
             <Text className="text learning-exercise-type-heading">{EXERCISE_TYPE_LOGIC[exerciseTypeId].Name}</Text>
             <View className="learning-exercise-type-heading-icon">
                 <ExerciseTypeIcon exerciseTypeId={exerciseTypeId} />
