2182849463a3d9d986d8a5258461ef057c607f16
Ayala Swisa <shani.ayala@gmail.com>
2026-08-06 11:36:25 +0300

    finalize app download page

--- apps/AyalasLanguageWeb/src/pages/DownloadApp.tsx
+++ apps/AyalasLanguageWeb/src/pages/DownloadApp.tsx
@@ -8,10 +8,10 @@ export function DownloadAppPage() {
             <PublicHeader />
             <div className="form-container">
                 <div className="about-header">
-                <div className="androidIcon" title="Android icons created by Swifticons - Flaticon" ></div><h1>Available on android!</h1>
+                <h1><div className="androidIcon" title="Android icons created by Swifticons - Flaticon" ></div>Available on Android!</h1>
                 </div>
                 <div className="form-label-row">
-                    <Link target="_blank" to="https://github.com/ayalas/AyalasLanguage/releases/latest">Download {BRAND_NAME} on Android</Link>
+                    Download the latest version of {BRAND_NAME} for Android from <Link target="_blank" to="https://github.com/ayalas/AyalasLanguage/releases/latest">its GitHub releases page</Link>. Click on the .apk file to download. You'll be prompted to install it, if you're browsing from an Android device. You may need to allow installation from unknown sources in your device settings.
                 </div>
             </div>
         </>);
