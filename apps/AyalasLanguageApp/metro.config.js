27d377fcc8f87173a1335ee946fd35b5df5098b1
Ayala Swisa <shani.ayala@gmail.com>
2026-08-06 07:25:59 +0300

    prepare for eas first build

--- apps/AyalasLanguageApp/metro.config.js
+++ apps/AyalasLanguageApp/metro.config.js
@@ -1,10 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require('path');
 
-const config = getDefaultConfig(__dirname, {
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot, {
   // This enables the new experimental PNPM support in Metro
   unstable_enableSymlinks: true, 
   unstable_enablePackageExports: true,
 });
+
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
 
 module.exports = withNativeWind(config, { input: "./global.css" });
