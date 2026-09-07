9c43c6b24e5075a450af842d56355be98b4283ea
Ayala Swisa <shani.ayala@gmail.com>
2026-09-07 08:40:44 +0300

    change replace-certs

--- replace-certs.ps1
+++ replace-certs.ps1
@@ -1,6 +1,6 @@
 $serverIP      = "[2a0f:f01:208:7e7::]"
 $sshKeyPath    = "C:\Users\ayala\.ssh\id_ed25519"
-$targetDir     = "/langapp-stack"
+$targetDir     = "/langapp-stack/temp"
 
 scp -i $sshKeyPath ./langapp-stack/Caddyfile admin@${serverIP}:${targetDir}/Caddyfile
 if (Test-Path ./langapp-stack/certs) {
